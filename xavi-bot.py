c92b1664698b7b2b8f31355d4eedee064fa6293d
baksteennz <christoeras@gmail.com>
2022-04-05 15:21:19 +1200

    stuff and things

--- xavi-bot.py
+++ xavi-bot.py
@@ -18,4 +18,12 @@ def cwriter():
 def main():
     Label(mainwindow, text="helllo world").grid()
     Label(mainwindow, text="hello2world").grid()
+    Button(mainwindow, text="submit", command=submit).grid()
+
+def submit():
+     entry = entry.get()
+     entry2 = entry2.get()
+     if entry:
+         
+
 main()
