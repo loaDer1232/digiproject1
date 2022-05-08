a4c3017ba45cd07b535a62cceb7c4e9d110a3c40
baksteennz <christoeras@gmail.com>
2022-05-08 21:33:33 +1200

    looks

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from tkinter import *
 
 startwindow = Tk()
 
-dropvalues = ["student", "location", "day", "date"]
+dropvalues = ["Student", "Location", "Day", "Date"]
 classlist = ["A", "B", "C"]
-typesofweather = ["sunny", "cloudy", "windy", "rainy", "stormy"]
+typesofweather = ["Sunny", "Cloudy", "Windy", "Rainy", "Stormy"]
 
 feilds = []
 rows = []
@@ -16,13 +16,13 @@ info = {}
 
 
 def start():
-    Label(startwindow, text="select function",
+    Label(startwindow, text="Select Function",
           font=("bold", 13)).grid(row=1, column=1)
-    Button(startwindow, text="daily report", command=daliyreportwindow,
+    Button(startwindow, text="Daily Report", command=daliyreportwindow,
            width=10, height=1).grid(row=2, column=1)
-    Button(startwindow, text="sherch", command=main,
+    Button(startwindow, text="Sherch", command=main,
            width=10, height=1).grid(row=2, column=2)
-    Button(startwindow, text="add student", command=addstudentwindow,
+    Button(startwindow, text="Add Student", command=addstudentwindow,
            width=10, height=1).grid(row=3, column=1)
     Button(startwindow, text="Quit", command=exit,
            width=10, height=1).grid(row=3, column=2)
@@ -38,9 +38,9 @@ def main():
     sherchby.set("Select an Option")
     fuck = OptionMenu(mainwindow, sherchby, *dropvalues)
     fuck.grid(row=1, column=2)
-    Label(mainwindow, text="sherch by").grid(row=1, column=1)
-    Label(mainwindow, text="sherch for").grid(row=2, column=1)
-    Button(mainwindow, text="submit", command=rer,
+    Label(mainwindow, text="Sherch By").grid(row=1, column=1)
+    Label(mainwindow, text="Sherch For").grid(row=2, column=1)
+    Button(mainwindow, text="Submit", command=rer,
            width=10, height=1).grid(row=3, column=2)
     global sherchbox
     sherchbox = Entry(mainwindow)
@@ -49,7 +49,7 @@ def main():
 
 def addstudentwindow():
     def makelist():
-        Label(mainwindow, text="submission successful",
+        Label(mainwindow, text="Submission Successful",
               foreground="green").grid(row=4, column=1)
         global newstudent
         # makes it so that i dont need to decaler every box a global varible and save mermoery somehow
@@ -59,27 +59,27 @@ def addstudentwindow():
     mainwindow = Tk()
     global writeas
     writeas = "role.csv"
-    Label(mainwindow, text="student last name").grid(row=1, column=1)
+    Label(mainwindow, text="Student Last Name").grid(row=1, column=1)
     studentbox1 = Entry(mainwindow)
     studentbox1.grid(row=1, column=2)
-    Label(mainwindow, text="student first name").grid(row=2, column=1)
+    Label(mainwindow, text="Student First Name").grid(row=2, column=1)
     studentbox2 = Entry(mainwindow)
     studentbox2.grid(row=2, column=2)
-    Label(mainwindow, text="student age").grid(row=3, column=1)
+    Label(mainwindow, text="Student Age").grid(row=3, column=1)
     studentbox3 = Spinbox(mainwindow, from_=14, to=18, wrap=True)
     studentbox3.grid(row=3, column=2)
-    Label(mainwindow, text="student class").grid(row=4, column=1)
+    Label(mainwindow, text="Student Class").grid(row=4, column=1)
     studentbox4 = StringVar(mainwindow)
     studentbox4.set(classlist[0])
     clas = OptionMenu(mainwindow, studentbox4, *classlist)
     clas.grid(row=4, column=2)
-    Button(mainwindow, text="submit", command=makelist,
+    Button(mainwindow, text="Submit", command=makelist,
            width=10, height=1).grid(row=5, column=2)
 
 
 def daliyreportwindow():
     def makelist():
-        Label(mainwindow, text="submission successful",
+        Label(mainwindow, text="Submission Successful",
               foreground="green").grid(row=6, column=1)
         global reportdata
         reportdata = [
@@ -93,49 +93,49 @@ def daliyreportwindow():
     mainwindow = Tk()
     global writeas
     writeas = "stuff.csv"
-    Label(mainwindow, text="location").grid(row=1, column=1)
+    Label(mainwindow, text="Location").grid(row=1, column=1)
     reportbox1 = Entry(mainwindow)
     reportbox1.grid(row=1, column=2)
-    Label(mainwindow, text="number of students").grid(row=2, column=1)
+    Label(mainwindow, text="Number of Students").grid(row=2, column=1)
     reportbox2 = Spinbox(mainwindow, from_=4, to=10, wrap=True)
     reportbox2.grid(row=2, column=2)
-    Label(mainwindow, text="day").grid(row=3, column=1)
+    Label(mainwindow, text="Day").grid(row=3, column=1)
     reportbox3 = Spinbox(mainwindow, from_=1, to=10, wrap=True)
     reportbox3.grid(row=3, column=2)
-    Label(mainwindow, text="weather").grid(row=4, column=1)
+    Label(mainwindow, text="Weather").grid(row=4, column=1)
     reportbox4 = StringVar(mainwindow)
     reportbox4.set(typesofweather[0])
     weather = OptionMenu(mainwindow, reportbox4, *typesofweather)
     weather.grid(row=4, column=2)
-    Label(mainwindow, text="class").grid(row=5, column=1)
+    Label(mainwindow, text="Class").grid(row=5, column=1)
     reportbox5 = StringVar(mainwindow)
     reportbox5.set(classlist[0])
     clas = OptionMenu(mainwindow, reportbox5, *classlist)
     clas.grid(row=5, column=2)
-    Button(mainwindow, text="submit", command=makelist,
+    Button(mainwindow, text="Submit", command=makelist,
            width=10, height=1).grid(row=6, column=2)
 
 
 def sherchoutput(info):
     results = Tk()
-    Label(results, text="results", font=("bold", 13)).grid(row=1, column=1)
+    Label(results, text="Results", font=("bold", 13)).grid(row=1, column=1)
     Label(results, text="\n".join(info)).grid(row=2, column=1)
     Label(results, text="\n".join(info.values())).grid(row=2, column=2)
-    Button(results, text="expaned", command=showmore).grid(row=3, column=2)
+    Button(results, text="Expaned", command=showmore).grid(row=3, column=2)
 
 
 def showmore():
-    if toopen == "student":
-        toopen = "stuff.csv"
-    elif toopen == "location" or "day":
-        toopen = "role.csv"
+    if toopen == dropvalues[0]:
+        toopen = dropvalues[1]
+    elif toopen == dropvalues[1 or 2 or 3]:
+        toopen = dropvalues[0]
     sherch()
 
 
 def sherch():
-    if toopen == "student":
+    if toopen == dropvalues[0]:
         file = "role.csv"
-    elif toopen == "location" or "day" or "date":
+    elif toopen == dropvalues[1 or 2 or 3]:
         file = "stuff.csv"
     with open(file) as mainfile:
         csv.reader(mainfile)
